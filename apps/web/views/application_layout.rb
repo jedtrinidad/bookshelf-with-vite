f68f3bcfb172e941193f32d569a79c7d47a863cd
Jed Cyril Trinidad <trinidadjedcyril@gmail.com>
2021-08-26 12:54:41 +0000

    Complete navbar layout

--- apps/web/views/application_layout.rb
+++ apps/web/views/application_layout.rb
@@ -2,19 +2,22 @@ module Web
   module Views
     class ApplicationLayout
       include Web::Layout
-      
+
+      TITLE_BAR_LINK_CLASS = "no-underline text-white px-2 py-4 hover:bg-white hover:text-red-500"
 
       def title_link
         link_to '/', class: 'no-underline' do
-          h1(class: 'text-3xl text-white font-extralight py-2') do
+          h1(class: 'text-3xl text-white font-extralight p-3') do
             "Hanami Bookshelf"
           end
         end 
       end
 
       def navbar
-        html.nav(class: 'flex justify-center p-3 bg-red-500') do
-          title_link + link_to('About', '/about', class: 'no-underline text-white py-2')
+        html.nav(class: 'flex justify-left p-3 bg-red-500') do
+          title_link + 
+            link_to('About', '/about', class: TITLE_BAR_LINK_CLASS) +
+            link_to('Books', '/books', class: TITLE_BAR_LINK_CLASS)
         end
       end
 
