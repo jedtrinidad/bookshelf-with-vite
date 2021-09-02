05a6425f5e19bca477c3c24ce8ea0c176b36ce3b
Jed Cyril Trinidad <trinidadjedcyril@gmail.com>
2021-09-02 11:39:20 +0000

    Add Form Validation

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -3,14 +3,28 @@ module Web
     module Books
       class Create
         include Web::Action
+        expose :books
+
+        params do
+          required(:book).schema do
+            required(:title).filled
+            required(:author).filled
+          end
+        end
 
         def call(params)
-          BookRepository.new.create(params[:book])
-
+          @books = BookRepository.new.all
           to_url = request.referrer
 
-          self.status = 303
-          self.headers.merge!({ 'Location' => to_url })
+          if params.valid?
+            BookRepository.new.create(params[:book])
+
+            self.status = 303
+            self.headers.merge!({ 'Location' => to_url })
+          else
+            self.status = 422
+            self.headers.merge!({ 'Location' => to_url })
+          end
         end
       end
     end
---
+++ apps/web/views/books/create.rb
@@ -0,0 +1,18 @@
+module Web
+  module Views
+    module Books
+      class Create
+        include Web::View
+        template 'books/index'
+
+        def add_book_form
+          form_for :book, '/books', class: 'grid grid-rows-2 grid-cols-2' do
+            text_field :title, placeholder: 'Title', class: 'p-2 m-1'
+            text_field :author, placeholder: 'Author', class: 'p-2 m-1'
+            submit 'Add Book', class: 'bg-green-500 text-white p-2 mt-2 rounded-xl col-span-full'
+          end
+        end
+      end
+    end
+  end
+end
