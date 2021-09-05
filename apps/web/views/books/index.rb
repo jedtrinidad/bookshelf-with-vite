35a692e62542698c88c03192626c44b934d3ea55
Jed Cyril Trinidad <trinidadjedcyril@gmail.com>
2021-09-05 09:13:59 +0000

    Fix bugs

--- apps/web/views/books/index.rb
+++ apps/web/views/books/index.rb
@@ -5,7 +5,7 @@ module Web
         include Web::View
 
         def add_book_form
-          form_for :book, '/books', class: 'grid grid-rows-2 grid-cols-2' do
+          form_for :book, routes.books_path, class: 'grid grid-rows-2 grid-cols-2' do
             text_field :title, placeholder: 'Title', class: 'p-2 m-1'
             text_field :author, placeholder: 'Author', class: 'p-2 m-1'
             submit 'Add Book', class: 'bg-green-500 text-white p-2 mt-2 rounded-xl col-span-full'
